b1c2fe22dc31aebe318294ff68ba106a11d3d7d1
Bourt <bout.theo@example.com>
2026-02-26 16:37:31 +0100

    Modification méthode repair_wall

--- station/space_station.py
+++ station/space_station.py
@@ -1,5 +1,5 @@
+from character.player import ENGINEER
 
-from character.player import *
 class SpaceStation: 
 
     def init(self):
@@ -8,35 +8,27 @@ class SpaceStation:
         self.oxygen_level=100
         self.oxygen_loss=5
 
-    def repair_wall(self):
-        if PlayerClass == ENGINEER:
-            self.wall_hp= self.wall_hp + 15
+    def repair_wall(self, player_class):
+        if player_class == ENGINEER:
+            self.wall_hp= min(self.max_wall_hp, self.wall_hp + 15)
         else:
-            self.wall_hp= self.wall_hp +10
+            self.wall_hp= min(self.max_wall_hp, self.wall_hp + 10)
 
     def damage_wall(self,damage):
-        self.wall_hp = max(0,self.wall_hp-damage)
+        self.wall_hp = max(0,self.wall_hp - damage)
 
     def reinforce_wall(self,amount):
         self.wall_hp += amount
         self.max_wall_hp += amount
 
     def improve_oxygen(self, reduction):
-        self.oxygen_loss=max(0,self.oxygen_loss-reduction)
+        self.oxygen_loss=max(0,self.oxygen_loss - reduction)
 
     def loss_oxygen(self):
-        self.oxygen_level = max(0,self.oxygen_level-self.oxygen_loss)
+        self.oxygen_level = max(0,self.oxygen_level - self.oxygen_loss)
 
     def is_destroyed(self) -> bool:
         return self.wall_hp <=0 or self.oxygen_level <=0
 
     def get_info_station(self):
-        return f"Mur: {self.wall_hp}/{self.max_wall_hp}\nOxygène: {self.oxygen_level}% (perte: -{self.oxygen_loss}% par tour) "
-
-
-    
-
-
-
-
-
+        return (f"Mur: {self.wall_hp}/{self.max_wall_hp}\nOxygène: {self.oxygen_level}% (perte: -{self.oxygen_loss}% par tour) ")
